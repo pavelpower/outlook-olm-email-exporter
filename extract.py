3c6c028f1f42d290c49ced34e30a3f63717ab91c
Pavel Akhmetchanov <pavel.power@gmail.com>
2021-01-15 19:08:54 +0300

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,41 +1,37 @@
 import os
+import csv
 import xml.etree.ElementTree as ET
 
-folders = ["Inbox", "Sent Items"]
-blacklist = ["asana.com", "reply", "noreply", "registration", "chime", "support@", "mailchimp", ".calendar", "=", "-_", "mailbox", "+","account","fatura","hizmetleri","musteri","marketing@", "help@", "sales"]
+appointments = []
+filename = "Calendar.xml"
+fieldnames = ['Summary', 'Organizer', 'StartTime', 'EndTime', 'Description', 'Attendees']
 
-email_list = []
-for folder in folders:
-    print("> Looking for: "+folder)
-    current_folder = "Local/com.microsoft.__Messages/"+folder
-    for filename in os.listdir(current_folder):
-        if not filename.endswith(".xml"):
-            print(current_folder+"/"+filename+" is not xml")
-            continue
+try:
+  tree = ET.parse(filename)
+except:
+  print(filename+" couldn't parsed")
+finally:
+  root = tree.getroot()
+  for appointment in root.iter('appointment'):
+    to_append = {}
+    if appointment.find('OPFCalendarEventCopySummary') is not None:
+      to_append['Summary'] = appointment.find('OPFCalendarEventCopySummary').text
+    if appointment.find('OPFCalendarEventCopyOrganizer') is not None:
+      to_append['Organizer'] = appointment.find('OPFCalendarEventCopyOrganizer').text
+    if appointment.find('OPFCalendarEventCopyStartTime') is not None:
+      to_append['StartTime'] = appointment.find('OPFCalendarEventCopyStartTime').text
+    if appointment.find('OPFCalendarEventCopyEndTime') is not None:
+      to_append['EndTime'] = appointment.find('OPFCalendarEventCopyEndTime').text
+    # if appointment.find('OPFCalendarEventCopyDescriptionPlain') is not None:
+    #   to_append['Description'] = appointment.find('OPFCalendarEventCopyDescriptionPlain').text
+    to_append['Attendees'] = []
+    attendeeList = appointment.find('OPFCalendarEventCopyAttendeeList')
+    if attendeeList is not None:
+      for attendee in attendeeList.findall('appointmentAttendee'):
+        to_append['Attendees'].append(attendee.attrib['OPFCalendarAttendeeAddress'])
+    appointments.append(to_append)
 
-        try:
-            tree = ET.parse(current_folder+"/"+filename)
-        except:
-            print(current_folder+"/"+filename+" couldn't parsed")
-        finally:
-            root = tree.getroot()
-            for item in root.iter('emailAddress'):
-                if 'OPFContactEmailAddressAddress' in item.attrib:
-                    if any(to_check in item.attrib['OPFContactEmailAddressAddress'].lower() for to_check in blacklist):
-                        #print(">>"+item.attrib['OPFContactEmailAddressAddress']+" is blacklisted")
-                        continue
-                    if any(item.attrib['OPFContactEmailAddressAddress'].lower() in s for s in email_list):
-                        #print(">>"+item.attrib['OPFContactEmailAddressAddress']+" exists")
-                        continue
-                    if 'OPFContactEmailAddressName' in item.attrib:
-                        to_append = item.attrib['OPFContactEmailAddressName']+" <"+item.attrib['OPFContactEmailAddressAddress'].lower()+">"
-                    else:
-                        to_append = item.attrib['OPFContactEmailAddressAddress']+" <"+item.attrib['OPFContactEmailAddressAddress'].lower()+">"
-                    email_list.append(to_append)
-
-# make every email unique
-email_list = list(set(email_list))
-
-f = open("export.txt", "w")
-f.write("\n".join(email_list).encode('utf8'))
+w = csv.DictWriter(open("output.csv", "w"), fieldnames=fieldnames, dialect='excel')
+w.writeheader()
+w.writerows(appointments)
 print("operation completed")
